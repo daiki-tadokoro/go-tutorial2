395833041bd1da5b3e8315e4bd06b6067c73eb72
daiki-tadokoro <>
2023-11-30 08:28:01 +0900

    feat: add create todo

--- main.go
+++ main.go
@@ -15,23 +15,26 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@exmaple.com"
-	// u.PassWord = "testpass"
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@exmaple.com"
+	u.PassWord = "testpass"
+	fmt.Println(u)
+
+	u.CreateUser()
+	// u, _ := models.GetUser(1)
 	// fmt.Println(u)
 
-	// u.CreateUser()
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
+	// u.Name = "test2"
+	// u.Email = "test2@example.com"
+	// u.UpdateUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	// u.DeleteUser()
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	user, _ := models.GetUser(2)
+	user.CreateTodo("Firs Todo")
 }
