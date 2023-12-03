b9a3ba1c74a0bd5fdefcecd8445585c913e94ca5
daiki-tadokoro <>
2023-12-03 10:17:11 +0900

    feat: add startMainServer

--- main.go
+++ main.go
@@ -1,72 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"todo/app/controllers"
 	"todo/app/models"
 )
 
 func main() {
-	// fmt.Println(config.Config.Port)
-	// fmt.Println(config.Config.SQLDriver)
-	// fmt.Println(config.Config.DbName)
-	// fmt.Println(config.Config.LogFile)
+	fmt.Println(models.Db)
 
-	// log.Println("test")
+	controllers.StartMainServer()
 
-	// fmt.Println(models.Db)
-
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@exmaple.com"
-	// u.PassWord = "testpass"
-	// fmt.Println(u)
-
-	// u.CreateUser()
-	// u, _ := models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.Name = "test2"
-	// u.Email = "test2@example.com"
-	// u.UpdateUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("Firs Todo")
-
-	// t, _ := models.GetTodo(1)
-	// fmt.Println(t)
-
-	// user, _ := models.GetUser(2)
-	// user.CreateTodo("Second Todo")
-
-	// todos, _ := models.GetTodos()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	// user, _ := models.GetUser(3)
-	// user.CreateTodo("Third Todo")
-
-	// user2, _ := models.GetUser(2)
-	// todos, _ := user2.GetTodosByUser()
-	// for _, v := range todos {
-	// 	fmt.Println(v)
-	// }
-
-	// t, _ := models.GetTodo(1)
-	// 変更前のTodoの表示
-	// fmt.Println(t)
-
-	// Todoの更新
-	// t.Content = "Update check"
-	// t.UpdateTodo()
-	// fmt.Println(t)
-
-	// Todoの削除
-	t, _ := models.GetTodo(1)
-	t.DeleteTodo()
 }
